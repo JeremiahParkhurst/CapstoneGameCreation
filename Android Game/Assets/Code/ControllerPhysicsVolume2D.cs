ec3ff3d1cddf1c03eacadb5a79ef065e236eb14e
John Doan <doanja@plu.edu>
2016-01-09 17:27:54 -0800

    Completed CharacterController2D & Others
    New:
    - JumpPlatform (for springing jumps)
    - ControllerPhysicsVolume2D (override character parameters to change how
    the character moves in different terrain)
    - BackgroundParallax (for moving backgrounds)
    Changes:
    - Completed all the methods in the CharacterController2D, such as
    platform handling, platforms knock back the player, player will be stuck
    to the platform (they do not need to move where the platform is going).
    - Person gameobject has a "Particle System" which basically leaves a
    path of particles where the player has recently traveled (will be used
    to demonstrate speedups, and other power ups)
    Bugs:
    - JumpPlatform does not work
    - Still need to test out how the character moves on slopes
    
    TODO:
    - Checkpoints, and health.

---
+++ Android Game/Assets/Code/ControllerPhysicsVolume2D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+* This class is used to overide the ControllerParameters2D parameters.
+* For example, this class is used to change how the player will move
+* in different areas of terrain such as water.
+*/
+public class ControllerPhysicsVolume2D : MonoBehaviour {
+
+    public ControllerParameters2D Parameters;
+
+}
---
+++ Android Game/Assets/Code/JumpPlatform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* https://www.youtube.com/watch?v=hkLAdo9ODDs&list=PLt_Y3Hw1v3QSFdh-evJbfkxCK_bjUD37n&index=12 */
+
+public class JumpPlatform : MonoBehaviour {
+
+    public float JumpMagnitude = 20;
+
+    public void ControllerEnter2d(CharacterController2D controller)
+    {
+        controller.SetVerticalForce(JumpMagnitude);
+    }
+
+}
