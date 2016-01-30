4a76ec9375ecfdbbbc3e332670023479e2a6b7b0
John Doan <doanja@plu.edu>
2016-01-29 17:52:46 -0800

    TurretAI
    New:
    - none
    
    Updates:
    - TurretAI (no  new progress)
    
    Bugs:
    - TurretAI lel

--- Android Game/Assets/Code/HomingProjectile.cs
+++ Android Game/Assets/Code/HomingProjectile.cs
@@ -1,21 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class HomingProjectile : MonoBehaviour {
 
-    private Player player;          // instance of the player class
-    public float Speed;             // the movement speed of this GameObject
+    public Player target;
 
     // Use this for initialization
     void Start () {
-        player = FindObjectOfType<Player>();
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+
         // Handles movement of this GameObject
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Speed * Time.deltaTime);
-        return;
+        transform.rotation = Quaternion.LookRotation(transform.forward);
+        transform.LookAt(target.transform.position);
+        transform.Translate(Vector3.forward * 1);
     }
 }
--- Android Game/Assets/Code/TurretAI.cs
+++ Android Game/Assets/Code/TurretAI.cs
@@ -1,51 +1,57 @@
 ﻿using UnityEngine;
 
-public class TurretAI : MonoBehaviour {
-
-    private Player player;          // instance of the player class
-    public LayerMask CollisionMask; // determines what this GameObject is colliding with
-    public float PlayerRange;       // the distance between the Player Object and this GameObject
-    public bool playerInRange;      // used to determine if the Player Object is in range of this GameObject
-
+public class TurretAI : MonoBehaviour, ITakeDamage, IPlayerRespawnListener
+{
+    public float Speed;                 // travel speed of this GameObject
     public float FireRate = 1;                  // cooldown time after firing a projectile
     public Projectile Projectile;               // this GameObject's projectile
-    public Transform ProjectileFireLocation;    // the location of which the projectile is fired at
-
     public GameObject DestroyedEffect;  // the destroyed effect
     public int PointsToGivePlayer;      // points awarded to the player upon killing this GameObject
     public Transform RespawnPosition;   // position where this GameObject is respawned at
+
+    private Player player;          // instance of the player class
+    public LayerMask CollisionMask; // determines what this GameObject is colliding with
+    public float detectionRange;       // the distance between the Player Object and this GameObject
+    public bool playerInRange;      // used to determine if the Player Object is in range of this GameObject
 
     private CharacterController2D _controller;  // has an instance of the CharacterController2D
     private Vector2 _direction;                 // the x-direction of this GameObject
     private Vector2 _startPosition;             // the initial spawn position of this GameObject
     private float _canFireIn;                   // the amount of time this GameObject can shoot projectiles
 
-    private Vector3 playerLocation;
+    public Transform ProjectileFireLocation;    // the location of which the projectile is fired at
+
 
     // Use this for initialization
     public void Start()
     {
         player = FindObjectOfType<Player>();
+        _controller = GetComponent<CharacterController2D>();
+        _direction = new Vector2(-1, 0);    // this GameObject will move the left upon initialization
         _startPosition = transform.position;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        // Variable used to determines if the CollisionMask overlaps with the Circle
-        playerInRange = Physics2D.OverlapCircle(transform.position, PlayerRange, CollisionMask);
-        playerLocation = player.transform.position;
-
         // Handles when this GameObject cannot shoot
         if ((_canFireIn -= Time.deltaTime) > 0)
             return;
 
+        // Variable used to determines if the CollisionMask overlaps with the Circle
+        playerInRange = Physics2D.OverlapCircle(transform.position, detectionRange, CollisionMask);
+
         // If the player is in range of the turret's attack range, shoot 
-        if (playerInRange)
+        if (playerInRange && !player.IsDead)
         {
             // Instantiates the projectile, and initilializes the speed, and direction of the projectile
             var projectile = (Projectile)Instantiate(Projectile, ProjectileFireLocation.position, ProjectileFireLocation.rotation);
-            projectile.Initialize(gameObject, playerLocation, _controller.Velocity);
+
+            //projectile.transform.rotation = Quaternion.LookRotation(player.transform.position);
+            //projectile.transform.LookAt(player.transform.position);
+            //projectile.transform.Translate(Vector3.forward * Time.deltaTime * 2);
+            projectile.Initialize(gameObject, _direction, _controller.Velocity); // projectile moves
+            
             _canFireIn = FireRate; // time frame, when projectiles can be shot from this GameObject
         }
     }
@@ -53,7 +59,7 @@ public class TurretAI : MonoBehaviour {
     // Method draws a sphere indicating the range of view of this GameObject
     public void OnDrawGizmosSelected()
     {
-        Gizmos.DrawSphere(transform.position, PlayerRange);
+        Gizmos.DrawSphere(transform.position, detectionRange);
     }
 
     /*
@@ -78,7 +84,6 @@ public class TurretAI : MonoBehaviour {
 
         // Effect played upon the death of this GameObject
         Instantiate(DestroyedEffect, transform.position, transform.rotation);
-
         gameObject.SetActive(false);                    // hides this GameObject
     }
 
