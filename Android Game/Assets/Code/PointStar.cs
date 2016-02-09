3e7631cd6f5a2410c5acd4fe5f70aa1f7563d6a6
John Doan <doanja@plu.edu>
2016-02-08 17:56:18 -0800

    Animations
    New:
    - Animations for Coin, Cannon, Spike Ball
    
    Updates:
    - PointStart script
    
    Bugs:
    - Null pointer in PointStart script, need to fix the renderer.

--- Android Game/Assets/Code/PointStar.cs
+++ Android Game/Assets/Code/PointStar.cs
@@ -13,6 +13,10 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
     public GameObject Effect;       // special effects upon colliding with the GameObject
     public int PointsToAdd = 10;    // the number of points the player is rewarded
     public AudioClip PickupSound;   // sound played when the player collides this GameObject
+    public Animator anim;
+    public SpriteRenderer rend;
+
+    private bool _isCollected;
 
     /*
     * @param other, the other GameObject
@@ -20,6 +24,9 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
     */
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if (_isCollected)
+            return;
+
         // Does nothing if another GameObject collides with this GameObject
         if (other.GetComponent<Player>() == null)
            return;
@@ -32,12 +39,21 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
         GameManager.Instance.AddPoints(PointsToAdd);
 
         // Handles effects
-        Instantiate(Effect, transform.position, transform.rotation);
-
-        gameObject.SetActive(false); // hides this GameObject
+        Instantiate(Effect, transform.position, transform.rotation);       
 
         // Floating text appears when picked up
         FloatingText.Show(string.Format("+{0}!", PointsToAdd), "PointStarText", new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
+
+        _isCollected = true;
+        anim.SetTrigger("Collect");           
+    }
+
+    // Method used to set this GameObject to false when animation is done
+    public void FinishAnimationEvent()
+    {
+        //gameObject.SetActive(false); // hides this GameObject
+        rend.enabled = false;
+        anim.SetTrigger("Reset");
     }
 
     /*
@@ -47,6 +63,7 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
     */
     public void OnPlayerRespawnInThisCheckpoint(Checkpoint checkpoint, Player player)
     {
+        _isCollected = false;
         gameObject.SetActive(true); // shows this GameObject
     }
 }
