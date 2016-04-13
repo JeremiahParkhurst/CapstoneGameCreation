23676ce7910c4df53929454066fc1956dfed2bba
John Doan <doanja@plu.edu>
2016-04-12 17:47:04 -0700

    Start Screen & Level Selection
    New:
    - start screen and level select scenes
    - MainMenus and LevelSelect scripts to transition user between levels.
    
    Updates:
    - Overlay buttons over Level 2
    
    Bugs:
    - Buttons are static

---
+++ Android Game/Assets/Code/Level Managers/MainMenus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenus : MonoBehaviour {
+
+    public string theLevel;
+
+	public void Play()
+    {
+        Application.LoadLevel(theLevel);
+    }  
+}
