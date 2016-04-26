fe93982f2a806110de5217908661c74fc61dc5c6
John Doan <doanja@plu.edu>
2016-04-25 22:48:49 -0700

    Elizabeth Knight
    New:
    - Elizabeth Knight prefab
    
    Updates:
    - ReflectProjectile scripts updated with code to destroy colliding
    projectiles from the player and return projectiles back.
    
    Bugs:
    - none

--- Android Game/Assets/Code/ReflectProjectiles.cs
+++ Android Game/Assets/Code/ReflectProjectiles.cs
@@ -2,25 +2,33 @@
 
 public class ReflectProjectiles : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Character Essentials    
+    private CharacterController2D _controller;  // has an instance of the CharacterController2D
+    private Vector2 _direction;                 // the x-direction of this GameObject
+    private Vector2 _startPosition;             // the initial spawn position of this GameObject
+    public Projectile Projectile;       // this GameObject's projectile
+    public Transform ProjectileFireLocation;    // the location of which the projectile is fired at
+
+    // Use this for initialization
+    void Start () {
+        _controller = GetComponent<CharacterController2D>();    // instance of Charactercontroller2D
+        _direction = new Vector2(-1, 0);                        // this GameObject will move the left upon initialization
+        _startPosition = transform.position;                    // starting position of this GameObject
+    }	
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Projectile>() == null)
             return;
 
-        var projectile = other.GetComponent<Projectile>();
+
+        var projectile = other.GetComponent<SimpleProjectile>();
         if (projectile != null && projectile.Owner.GetComponent<Player>() != null)
         {
-            projectile.transform.position = Vector3.Reflect(-projectile.Direction, Vector3.right);
+            DestroyObject(other);            
+            // Instantiates the projectile, and initilializes the speed, and direction of the projectile
+            projectile = (SimpleProjectile)Instantiate(Projectile, ProjectileFireLocation.position, ProjectileFireLocation.rotation);
+            projectile.Initialize(gameObject, _direction, _controller.Velocity);
         }
     }
 }
