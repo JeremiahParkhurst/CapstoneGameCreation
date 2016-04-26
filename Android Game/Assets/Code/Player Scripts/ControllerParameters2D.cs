bc8f7914da1f1ada04ec87282afa1990fd6a59e7
John Doan <doanja@plu.edu>
2016-04-25 17:55:55 -0700

    Tests with Reflecting Projectiles
    New:
    - added some code to ReflectProjectiles.cs
    
    Updates:
    - none
    
    Bugs:
    - reflected projectiles get sent to (0,0)

--- Android Game/Assets/Code/Player Scripts/ControllerParameters2D.cs
+++ Android Game/Assets/Code/Player Scripts/ControllerParameters2D.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 
 [Serializable]
 public class ControllerParameters2D {
--- Android Game/Assets/Code/ReflectProjectiles.cs
+++ Android Game/Assets/Code/ReflectProjectiles.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ReflectProjectiles : MonoBehaviour {
 
@@ -12,4 +11,16 @@ public class ReflectProjectiles : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Projectile>() == null)
+            return;
+
+        var projectile = other.GetComponent<Projectile>();
+        if (projectile != null && projectile.Owner.GetComponent<Player>() != null)
+        {
+            projectile.transform.position = Vector3.Reflect(-projectile.Direction, Vector3.right);
+        }
+    }
 }
