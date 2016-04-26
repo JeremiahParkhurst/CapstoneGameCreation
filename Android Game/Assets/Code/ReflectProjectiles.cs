faeadaadcd0c645566021a930a559beb3992fb60
John Doan <doanja@plu.edu>
2016-04-26 11:14:17 -0700

    Elizabeth Knight
    New:
    - none
    
    Updates:
    - Projectile Effects and sound for ReflectProjectiles class

--- Android Game/Assets/Code/ReflectProjectiles.cs
+++ Android Game/Assets/Code/ReflectProjectiles.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 
+/*
+* Class destroys incoming Projectiles from the Player upon colliding with this GameObject
+* and instantiates a projectile back.
+*/
 public class ReflectProjectiles : MonoBehaviour {
 
     // Character Essentials    
     private CharacterController2D _controller;  // has an instance of the CharacterController2D
     private Vector2 _direction;                 // the x-direction of this GameObject
     private Vector2 _startPosition;             // the initial spawn position of this GameObject
-    public Projectile Projectile;       // this GameObject's projectile
+
+    // Projectile
+    public Projectile Projectile;               // this GameObject's projectile
     public Transform ProjectileFireLocation;    // the location of which the projectile is fired at
+    public GameObject ProjectileSpawnEffect;// effect played when spawning the projectile
+
+    // Sound
+    public AudioClip BlockProjectileSound;            // the sound when this GameObject shoots a projectile
 
     // Use this for initialization
     void Start () {
@@ -16,16 +26,32 @@ public class ReflectProjectiles : MonoBehaviour {
         _startPosition = transform.position;                    // starting position of this GameObject
     }	
 
+    /*
+    * @param other, the other GameObject colliding with this GameObject
+    * Function that handles what happens on collision.
+    */
     public void OnTriggerEnter2D(Collider2D other)
     {
+        // Does nothing if other is not a projectile
         if (other.GetComponent<Projectile>() == null)
             return;
 
-
+        // If other is an instance of a SimpleProjectile
         var projectile = other.GetComponent<SimpleProjectile>();
+
+        // Checks to see if the owner of the projectile is the player
         if (projectile != null && projectile.Owner.GetComponent<Player>() != null)
         {
-            DestroyObject(other);            
+            // Handles projectile effects
+            if (ProjectileSpawnEffect != null)
+                Instantiate(ProjectileSpawnEffect, ProjectileFireLocation.transform.position, ProjectileFireLocation.transform.rotation);
+
+            // Sound
+            if (BlockProjectileSound != null)
+                AudioSource.PlayClipAtPoint(BlockProjectileSound, transform.position);
+
+            DestroyObject(other); // destroys the projectile
+
             // Instantiates the projectile, and initilializes the speed, and direction of the projectile
             projectile = (SimpleProjectile)Instantiate(Projectile, ProjectileFireLocation.position, ProjectileFireLocation.rotation);
             projectile.Initialize(gameObject, _direction, _controller.Velocity);
