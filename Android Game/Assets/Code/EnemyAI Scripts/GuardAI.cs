94c850809a64f12ddb11ba581f74012eb000ee1f
John Doan <doanja@plu.edu>
2016-04-25 17:40:22 -0700

    Updated Elizabeth Mage
    New:
    - ReflectProjectile Script (dummied)
    - Elizabeth Knight Sprites
    
    Updates:
    - Sprites, prefab for Elizabeth Mage
    - Changed MageElizabethAI2 to GuardAI
    - Moves RotPortal and RotMonster animations and their controllers to the
    new animations folder.

--- Android Game/Assets/Code/EnemyAI Scripts/GuardAI.cs
+++ Android Game/Assets/Code/EnemyAI Scripts/GuardAI.cs
@@ -7,52 +7,48 @@
 * detected, this GameObject will spawn a projectile that will follow the Player. The turret has a set 
 * amount of health, which can be decremented by the Player's projectiles. 
 */
-public class MageElizabethAI2 : MonoBehaviour, ITakeDamage, IPlayerRespawnListener
+public class GuardAI : MonoBehaviour, ITakeDamage, IPlayerRespawnListener
 {
-    /* From PathedProjectileSpawner */
-    public Transform Destination;           // the location where the projectile will travel to
+    // Parameters
+    public float ProjectileTravelSpeed;     // the travel speed of the projectile towards its destination
+    public float FireRate;                  // the rate of shots the projectile will be fired at
+    private float Cooldown;                 // the cooldown before firing another shot
+    public Transform ProjectileDestination; // the location where the projectile will travel to
+    public Transform ProjectileFireLocation;
     public PathedProjectile Projectile;     // the projectile shot
     public GameObject ProjectileSpawnEffect;// effect played when spawning the projectile
+    public GameObject DestroyedEffect;      // the destroyed effect
+    public int PointsToGivePlayer;          // points awarded to the player upon killing this GameObject      
 
-    public float Speed;                     // the travel speed of the projectile towards its destination
-    public float FireRate;                  // the rate of shots the projectile will be fired at
-    public AudioClip SpawnProjectileSound;  // the sound of the projectile spawning
-
-    private float Cooldown;                 // the cooldown before firing another shot
-
-
-    public GameObject DestroyedEffect;  // the destroyed effect
-    public int PointsToGivePlayer;      // points awarded to the player upon killing this GameObject
-    public Transform RespawnPosition;   // position where this GameObject is respawned at
+    // Character Parameters
+    private CharacterController2D _controller;  // has an instance of the CharacterController2D
     private Vector2 _startPosition;     // the initial spawn position of this GameObject
 
-    /* From BirdAI */
+    // Player Checks
     private Player player;          // instance of the player class
     public LayerMask CollisionMask; // determines what this GameObject is colliding with
-    //public float MovementSpeed;             // the movement speed of this GameObject
     public float PlayerRange;       // the distance between the Player Object and this GameObject
     public bool playerInRange;      // used to determine if the Player Object is in range of this GameObject
-
-    private CharacterController2D _controller;  // has an instance of the CharacterController2D
 
     // Health
     public int MaxHealth = 100;                     // maximum health of the this GameObject
     public int Health { get; private set; }         // this GameObject's current health    
 
     // Sound
-    public AudioClip EnemyDestroySound;    // sound played when this GameObject is destroyed
+    public AudioClip EnemyDestroySound;     // sound played when this GameObject is destroyed
+    public AudioClip SpawnProjectileSound;  // the sound of the projectile spawning
 
     // Use this for initialization
-    void Start () {
-        Cooldown = FireRate;
-
+    void Start()
+    {
         player = FindObjectOfType<Player>();
         _startPosition = transform.position;
         Health = MaxHealth;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         // Spawner code
         if ((Cooldown -= Time.deltaTime) > 0)
             return;
@@ -61,19 +57,17 @@ public class MageElizabethAI2 : MonoBehaviour, ITakeDamage, IPlayerRespawnListen
 
         // Variable used to determines if the CollisionMask overlaps with the Circle
         playerInRange = Physics2D.OverlapCircle(transform.position, PlayerRange, CollisionMask);
-        
+
         // If the Player Object is in range of this GameObject, and they are facing away, move this GameObject towards the PlayerObject
         if (playerInRange)
         {
-            // Handles movement of this GameObject
-            //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MovementSpeed * Time.deltaTime);
-            //return;
-            var projectile = (PathedProjectile)Instantiate(Projectile, transform.position, transform.rotation); // initializes the projectile
-            projectile.Initialize(Destination, Speed); // moving the projectile
+            // Projectile
+            var projectile = (PathedProjectile)Instantiate(Projectile, ProjectileFireLocation.transform.position, ProjectileFireLocation.transform.rotation);
+            projectile.Initialize(ProjectileDestination, ProjectileTravelSpeed);
 
             // Handles projectile effects
             if (ProjectileSpawnEffect != null)
-                Instantiate(ProjectileSpawnEffect, transform.position, transform.rotation);
+                Instantiate(ProjectileSpawnEffect, ProjectileFireLocation.transform.position, ProjectileFireLocation.transform.rotation);
 
             // Sound
             if (SpawnProjectileSound != null)
@@ -84,11 +78,11 @@ public class MageElizabethAI2 : MonoBehaviour, ITakeDamage, IPlayerRespawnListen
     // Visual indicator for line of travel for the projectile & PlayerRange
     public void OnDrawGizmos()
     {
-        if (Destination == null)
+        if (ProjectileDestination == null)
             return;
 
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, Destination.position);
+        Gizmos.DrawLine(transform.position, ProjectileDestination.position);
 
         Gizmos.color = Color.cyan;
         Gizmos.DrawSphere(transform.position, PlayerRange);
@@ -134,11 +128,7 @@ public class MageElizabethAI2 : MonoBehaviour, ITakeDamage, IPlayerRespawnListen
     */
     public void OnPlayerRespawnInThisCheckpoint(Checkpoint checkpoint, Player player)
     {
-        //transform.position = _startPosition;            // initial position of this GameObject
         gameObject.SetActive(true);                     // shows this GameObject
-        //transform.position = RespawnPosition.position;  // position where this GameObject is respawned at
-
-        // Resets health
         Health = MaxHealth;                             // sets current health to the GameObject's max health
     }
 }
